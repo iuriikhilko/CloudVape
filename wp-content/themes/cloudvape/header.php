408908da288c294ae6baf3fcf894f7b71414c864
Iurii Khilko <ikhilko@amcbridge.com>
2018-04-30 13:17:45 +0300

    update translate

--- wp-content/themes/cloudvape/header.php
+++ wp-content/themes/cloudvape/header.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?php is_home() ? bloginfo('description') : wp_title(''); ?></title>
     <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
-    <?php wp_head(); ?><script>  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');  ga('create', 'UA-103177951-1', 'auto');  ga('send', 'pageview');</script>
+    <?php wp_head(); ?>
+    <script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');  ga('create', 'UA-103177951-1', 'auto');  ga('send', 'pageview');</script>
   </head>
   <body>
     <header style="border-bottom: 1px solid #000;">
